b89a2008daf4ac87a4c1251d2329074e25ac67ea
rakib-renessainfosystems <Renessa@DESKTOP-NJE6SAS>
2025-01-13 12:19:06 +0600

    Base_push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sample_task_ionic',
+  webDir: 'www'
+};
+
+export default config;
